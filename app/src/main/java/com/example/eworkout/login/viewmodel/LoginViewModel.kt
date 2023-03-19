ea0bab3fae46814e47fd5eef29494a706fc8b216
Trieu Luu <trieukim1410@gmail.com>
2023-03-19 20:05:26 +0700

    loginfunction

--- app/src/main/java/com/example/eworkout/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/eworkout/login/viewmodel/LoginViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.eworkout.login.model.LoginState
-import com.example.eworkout.signup.model.SignupState
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
