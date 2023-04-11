8b4e256c522cf0e13a6bbf668b1d30f16cf19be6
Trieu Luu <trieukim1410@gmail.com>
2023-04-11 14:26:55 +0700

    recyclerview

--- app/src/main/java/com/example/eworkout/training/adapter/set_recycler_adapter.kt
+++ app/src/main/java/com/example/eworkout/training/adapter/set_recycler_adapter.kt
@@ -1,6 +1,22 @@
 package com.example.eworkout.training.adapter
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class set_recycler_adapter: RecyclerView.Adapter<set_recycler_adapter.ViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): set_recycler_adapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: set_recycler_adapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
