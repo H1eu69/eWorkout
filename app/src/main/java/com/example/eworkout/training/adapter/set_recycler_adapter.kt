e3bbd082c6c293efe9719a73636953e63116618d
Trieu Luu <trieukim1410@gmail.com>
2023-04-11 14:23:57 +0700

    recyclerview

---
+++ app/src/main/java/com/example/eworkout/training/adapter/set_recycler_adapter.kt
@@ -0,0 +1,6 @@
+package com.example.eworkout.training.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+
+class set_recycler_adapter: RecyclerView.Adapter<set_recycler_adapter.ViewHolder>() {
+}
