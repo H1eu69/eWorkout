989e9f96c96d422ba9a3eff2ad542b17b0a62543
Trieu Luu <trieukim1410@gmail.com>
2023-04-11 18:53:30 +0700

    recyclerview

--- app/src/main/java/com/example/eworkout/training/adapter/set_recycler_adapter.kt
+++ app/src/main/java/com/example/eworkout/training/adapter/set_recycler_adapter.kt
@@ -1,5 +1,6 @@
 package com.example.eworkout.training.adapter
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,33 +8,54 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eworkout.R
+import com.example.eworkout.databinding.FragmentSetsItemBinding
 import com.example.eworkout.training.model.Set
+import kotlinx.coroutines.NonDisposableHandle
+import kotlinx.coroutines.NonDisposableHandle.parent
 
-class set_recycler_adapter(val set: List<Set>): RecyclerView.Adapter<set_recycler_adapter.ViewHolder>() {
+class set_recycler_adapter(val list: List<Set>): RecyclerView.Adapter<set_recycler_adapter.ViewHolder>() {
 
+    inner class ViewHolder(val binding: FragmentSetsItemBinding) : RecyclerView.ViewHolder(binding.root)
+    {
+        fun bind(set: Set)
+        {
+            set.apply {
+                val bundle = Bundle().apply {
+                    putString("exercise_id", setId)
+                }
+                binding.textViewSetName.text = setName
+                binding.textViewTotalExercises.text = totalExercises
+                binding.texViewTotalTime.text = totalTime
+                /*binding.exerciseInformationBtn.setOnClickListener {
+                    listener.onClick(bundle)
+                }
+                if(image != ""){
+                    val radius = binding.root.context.resources.getDimensionPixelSize(R.dimen.corner_radius)
+                    Glide.with(binding.root.context).load(image)
+                        .transform(RoundedCorners(radius))
+                        .centerCrop()
+                        .into(binding.shapeableImageView)
+                }*/
+
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.fragment_sets_item,parent,false)
-        return ViewHolder(itemView)
+        val binding = FragmentSetsItemBinding.inflate(LayoutInflater.from(parent.context),
+            parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val setTaken = set[position]
-        holder._setName.text = setTaken.setName
-        holder._totalExercise.text = setTaken.totalExercises
-        holder._totalTime.text = setTaken.totalTime
+        val setTaken = list[position]
+        holder.bind(setTaken)
     }
 
     override fun getItemCount(): Int {
-        return set.size
+        return list.size
     }
 
-    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
-        val _setName: TextView = itemView.findViewById(R.id.textView_SetName)
-        val _totalExercise: TextView = itemView.findViewById(R.id.textView_TotalExercises)
-        val _totalTime: TextView = itemView.findViewById(R.id.texView_TotalTime)
-        val _image: ImageView = itemView.findViewById(R.id.Set_imageView)
-    }
 
     /*val list: List<Exercise>,
     val listener: ExercisesOnClickListener) : RecyclerView.Adapter<ExercisesAdapter.ViewHolder>() {
